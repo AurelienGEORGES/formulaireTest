51b21cae24489f69daaf5f26872efc139268283a
kevin mancelle <mancelle.kevin28@gmail.com>
2022-01-04 15:39:59 +0100

    third commit

--- contact.php
+++ contact.php
@@ -1,5 +1,45 @@
 <?php
 
+if  ( isset ($_GET['Nom'])){
 
+    $Nom =$_GET['Nom'];
 
+}
+
+if  ( isset ($_GET['Prénom'])){
+
+    $Prénom =$_GET['Prénom'];
+
+   
+}
+if  ( isset ($_GET['mailNom'])){
+
+    $mailNom =$_GET['mailNom'];
+
+}
+
+if  ( isset ($_GET['phonenumber'])){
+
+    $phonenumber = $_GET['phonenumber'];
+
+    
+}
+if  ( isset ($_GET['address'])){
+
+    $address =$_GET['address'];
+
+   
+}
+
+$formulaire =  [
+    "Nom" => $Nom,
+    "Prénom" => $Prénom,
+    "mailNom"=> $mailNom,
+    "phonenumber"=> $phonenumber,
+    "address"=> $address,
+];
+
+print_r($formulaire);
 ?>
+
+
